7818c6256048214e32abd58b1cccc33c7252a427
rameez_hameed <rameez012001@gmail.com>
2025-08-16 16:30:22 +0530

    move things to data folder

--- src/Components/jsx/Activities.js
+++ src/Components/jsx/Activities.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { reads, articles } from "../assets/data/data";
+import { reads, articles, websites } from "../assets/data/data";
 function Activities() {
   return (
     <div className="activities">
@@ -9,12 +9,10 @@ function Activities() {
         </div>
         <div className="content">
           <p className="paragraph">
-            When I first got into reading, it was all about romance novels.
-            They were giving me the stuff I wasn’t living. And when I didn’t
-            know what to pick up next, I just hit up Amazon, looking at
-            those five-star deals and solid reviews. That’s how I tripped into
-            my first crime thrillers, kept me hooked, and then I even slid into
-            some classics.
+            At first I was all about those romance novels. They
+            were giving me the stuff I wasn’t living. Some how I ended up
+            reading my first crime thrillers and even
+            slid into some classics.
           </p>
           <div>
             <p>
@@ -52,12 +50,31 @@ function Activities() {
       </div>
       <div className="content-container">
         <div className="content-header">
+          <h1>Websites</h1>
+        </div>
+        <div className="content">
+          <p className="paragraph">Some oddly cool websites</p>
+          <ul>
+            {websites.map((website, idx) => (
+              <a
+                key={idx}
+                href={website.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {website.name}
+              </a>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="content-container">
+        <div className="content-header">
           <h1>Articles</h1>
         </div>
         <div className="content">
-          <p className="paragraph">
-            Articles that you might find interesting.
-          </p>
+          <p className="paragraph">Articles that worth sharing</p>
           <ul>
             {articles.map((article, idx) => (
               <a
